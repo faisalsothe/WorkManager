493b37e92f857ee3793a076dd3a5e9462e803986
Faisal Sothe <fs236963@gmail.com>
2023-10-13 23:42:30 +0530

    Update WelcomePage.jsx

--- src/app/profile/user/WelcomePage.jsx
+++ src/app/profile/user/WelcomePage.jsx
@@ -1,15 +1,12 @@
 'use client'
-// src/components/WelcomePage.js
-import UserContext from "@/context/userContext";
-import React, { useContext } from "react";
+import React from "react";
 
 function WelcomePage() {
-    const context=useContext(UserContext);
 
   return (
     <div className="bg-blue-500 min-h-screen flex items-center justify-center">
       <div className="text-center text-white">
-        <h1 className="text-5xl font-semibold mb-4">Welcome, {context.user.name}</h1>
+        <h1 className="text-5xl font-semibold mb-4">Welcome</h1>
         <p className="text-xl mb-8">
         Welcome to your new task manager! We understand that managing your tasks and responsibilities efficiently is crucial in today's fast-paced world. Whether you're a busy professional, a student juggling multiple assignments, or simply looking to stay organized in your daily life, our task manager is here to simplify your journey.<br/>
 With our user-friendly interface and powerful features, you'll be able to effortlessly create, prioritize, and track your tasks, ensuring that nothing falls through the cracks. Say goodbye to the stress of forgotten to-dos and missed deadlines, and say hello to a more organized and productive you.
